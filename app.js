19407b9dad07ba7a1264a7fa03d345a4e5f66559
Olei Eto <teneeto@gmail.com>
2019-10-08 15:12:32 +0100

    connect to mongoose and get all books

--- app.js
+++ app.js
@@ -1,15 +1,20 @@
 const express = require('express');
+const mongoose = require('mongoose');
 
 const server = express();
-
-const port = process.env.PORT || 3000;
-
+const db = mongoose.connect('mongodb://localhost/bookAPI');
 const bookRouter = express.Router();
+const port = process.env.PORT || 3000;
+const Book = require('./models/book');
 
 bookRouter.route('/books')
   .get((req, res) => {
-    const response = { Hello: 'This is my API approach' };
-    res.json(response);
+    Book.find((err, books) => {
+      if (err) {
+        return res.send(err);
+      }
+      return res.json(books);
+    })
   });
 
 server.use('/api/v1', bookRouter);
@@ -18,7 +23,7 @@ server.get('/', (req, res) => {
   res.send('Welcome to API tests');
 });
 
-/* eslint-disable-next-line no-console */
 server.listen(port, () => {
+  /* eslint-disable-next-line no-console */
   console.log(`Running on Port ${port}`);
 });
