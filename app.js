dc05d3ed614620412248895fd2f0659794d19046
Olei Eto <teneeto@gmail.com>
2019-10-10 16:54:40 +0100

    add update with PUT verb

--- app.js
+++ app.js
@@ -4,47 +4,13 @@ const bodyParser = require('body-parser');
 
 const server = express();
 const db = mongoose.connect('mongodb://localhost/bookAPI');
-const bookRouter = express.Router();
 const port = process.env.PORT || 3000;
 const Book = require('./models/book');
+const bookRouter = require('./routes/bookRouter')(Book);
 
 server.use(bodyParser.urlencoded({ extended: true }));
 server.use(bodyParser.json());
 
-bookRouter.route('/books')
-  .post((req, res) => {
-    const book = new Book(req.body);
-    book.save();
-    return res.status(201).json(book);
-
-  })
-
-  //to get all books in db
-  .get((req, res) => {
-    // const {query} = req;
-    const query = {};
-
-    //to get and filter by genre
-    if (req.query.genre) {
-      query.genre = req.query.genre;
-    }
-    Book.find(query, (err, books) => {
-      if (err) {
-        return res.send(err);
-      }
-      return res.json(books);
-    })
-  });
-
-bookRouter.route('/books/:bookId')
-  .get((req, res) => {
-    Book.findById(req.params.bookId, (err, book) => {
-      if (err) {
-        return res.send(err);
-      }
-      return res.json(book);
-    })
-  });
 
 server.use('/api/v1', bookRouter);
 
