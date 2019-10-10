06f0de9d052edd1f9d025f334b3d511df2a5320c
Olei Eto <teneeto@gmail.com>
2019-10-10 14:42:18 +0100

    add post verb to receive from req.body

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
 
 const server = express();
 const db = mongoose.connect('mongodb://localhost/bookAPI');
@@ -7,10 +8,23 @@ const bookRouter = express.Router();
 const port = process.env.PORT || 3000;
 const Book = require('./models/book');
 
+server.use(bodyParser.urlencoded({ extended: true }));
+server.use(bodyParser.json());
+
 bookRouter.route('/books')
+  .post((req, res) => {
+    const book = new Book(req.body);
+    console.log(book);
+    return res.json(book);
+    
+  })
+
+  //to get all books in db
   .get((req, res) => {
     // const {query} = req;
     const query = {};
+
+    //to get and filter by genre
     if (req.query.genre) {
       query.genre = req.query.genre;
     }
@@ -22,7 +36,7 @@ bookRouter.route('/books')
     })
   });
 
-  bookRouter.route('/books/:bookId')
+bookRouter.route('/books/:bookId')
   .get((req, res) => {
     Book.findById(req.params.bookId, (err, book) => {
       if (err) {
