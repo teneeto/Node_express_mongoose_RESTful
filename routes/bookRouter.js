935c7d74facaa3601b42143502c276ed7b8da98f
Olei Eto <teneeto@gmail.com>
2019-10-11 00:58:58 +0100

    add patch and delete verbs

--- routes/bookRouter.js
+++ routes/bookRouter.js
@@ -39,20 +39,47 @@ function routes(Book) {
       }
       return res.sendStatus(404);
     })
-
   })
-
   bookRouter.route('/books/:bookId')
-    .get((req, res) => res.json(book))
+    .get((req, res) => res.json(req.book))
     .put((req, res) => {
-        const { book } = req;
-        book.title = req.body.title;
-        book.author = req.body.author;
-        book.genre = req.body.genre;
-        book.read = req.body.read;
-        book.save();
+      const { book } = req;
+      book.title = req.body.title;
+      book.author = req.body.author;
+      book.genre = req.body.genre;
+      book.read = req.body.read;
+      req.book.save((err) => {
+        if (err) {
+          return res.send(err);
+        }
         return res.json(book);
       })
+    })
+    .patch((req, res) => {
+      const { book } = req;
+      if (req.body._id) {
+        delete req.body._id;
+      }
+      Object.entries(req.body ).forEach((item) => {
+        const key = item[0];
+        const value = item[1];
+        book[key] = value;
+      });
+      req.book.save((err) => {
+        if (err) {
+          return res.send(err);
+        }
+        return res.json(book);
+      })
+    })
+    .delete((req,res)=>{
+      req.body.remove((err)=>{
+        if (err){
+          return res.send(err);
+        }
+        return res.status(204);
+      })
+    })
 
   return bookRouter;
 }
